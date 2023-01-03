eda5d98468a087631738299f8e4507d45eabb155
Jbleezy <thejbirdjustin@yahoo.com>
2023-01-03 15:34:59 -0800

    Add config settings

--- src/main/java/com/dynamicentityhider/DynamicEntityHiderConfig.java
+++ src/main/java/com/dynamicentityhider/DynamicEntityHiderConfig.java
@@ -3,9 +3,22 @@ package com.dynamicentityhider;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup("example")
 public interface DynamicEntityHiderConfig extends Config
 {
-
+	@Range(
+			min = 1,
+			max = 2000
+	)
+	@ConfigItem(
+			keyName = "showOthersAmount",
+			name = "Show Others Amount",
+			description = "Maximum amount of other players to show"
+	)
+	default int showOthersAmount()
+	{
+		return 100;
+	}
 }
